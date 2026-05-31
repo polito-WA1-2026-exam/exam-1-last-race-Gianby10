2ac0bac1886b4d86d1dc25001eb8cfb57c22d395
gianbattista vivolo <gianbattista.vivolo@gmail.com>
2026-05-31 20:35:04 +0200

    add auth routes and islogged in middleware

--- server/index.js
+++ server/index.js
@@ -41,7 +41,49 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.authenticate("session"));
 
-app.get("/", (req, res) => {
+// Check if user is logged in middleware
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    // If user is authenticated, proceed with the request
+    return next();
+  }
+  // If user is not loggedin , send the user a 401 unauthorized message
+  return res.status(401).json({ error: "Unauthorized" });
+};
+
+// Login
+app.post("/api/sessions", function (req, res, next) {
+  // Try to authenticate using local strategy
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      // If user does not exists, print the error message specified in the passportjs localstartegy func
+      return res.status(401).json({ error: info });
+    }
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      // If login is successfully, send the user got in the verify localstrategy func
+      return res.json(req.user);
+    });
+  })(req, res, next);
+});
+
+// Check if the user is logged in
+app.get("/api/sessions/current", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+  } else res.status(401).json({ error: "Not authenticated" });
+});
+
+// Logout
+app.delete("/api/sessions/current", (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+app.get("/", isLoggedIn, (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
