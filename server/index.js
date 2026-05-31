bc3dc70e07d908229138173115a9ccf34ea37115
gianbattista vivolo <gianbattista.vivolo@gmail.com>
2026-05-31 20:10:49 +0200

    implement session auth using passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,45 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
+import passport from "passport";
+import LocalStrategy from "passport-local";
 const app = new express();
 const PORT = 3001;
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    async function verify(email, password, cb) {
+      const user = await login(email, password);
+
+      if (!user) {
+        return cb(null, false, "Invalid credentials");
+      }
+      return cb(null, user);
+    },
+  ),
+);
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (user, cb) {
+  return cb(null, user);
+});
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.authenticate("session"));
 
 app.get("/", (req, res) => {
   res.status(200).json({ status: "OK" });
