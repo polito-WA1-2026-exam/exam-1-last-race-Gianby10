f675108e9e6d4f5595e16290fb77b3e75b9ad988
gianbattista vivolo <gianbattista.vivolo@gmail.com>
2026-06-19 19:43:12 +0200

    fix leaderboard

--- server/games-dao.js
+++ server/games-dao.js
@@ -38,7 +38,7 @@ export const getLinesStations = () => {
 
 export const getLeaderboard = () => {
   return new Promise((resolve, reject) => {
-    const query = `SELECT MAX(score) as score, users.name AS user_name, users.id AS user_id FROM games JOIN users ON user_id = users.id GROUP BY user_id ORDER BY score DESC`;
+    const query = `SELECT MAX(score) as score, users.name AS user_name, users.id AS user_id FROM games JOIN users ON user_id = users.id WHERE score IS NOT NULL GROUP BY user_id ORDER BY score DESC`;
     db.all(query, (err, rows) => {
       if (err) {
         reject(err);
